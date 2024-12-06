5dc77b4ffff57a9acd9be269619718a5fb773c15
Hans <hansvekoni@gmail.com>
2024-12-06 09:49:11 -0300

    update hh.config vars for validate and work with alchemy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+//npm install --save-dev @nomiclabs/hardhat-etherscan
+//Para verificar contratos
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY") || "";
+const PRIVATE_KEY = vars.get("PRIVATE_KEY") || "";
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY") || "";//para verificar contratos
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY, // Configuración para verificación
+  },
 };
 
 export default config;
